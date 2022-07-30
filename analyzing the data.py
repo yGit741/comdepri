7101fb833eab2417c3f1f77074c45d5ea298bbe2
Galit <gmenczel@gmail.com>
2022-07-30 14:11:12 +0300

    changed efficiency of key filter function

--- analyzing the data.py
+++ analyzing the data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8dbab2a0e017b4273ac8f945ad917961fa4c2d0702f4989872514b5bfe809f4
-size 2740
+oid sha256:9d53076b27942625e4fd69e97bddf1c27e9407a2a495499d71b7cc163ef6f627
+size 4249
